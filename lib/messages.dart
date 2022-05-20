c9fc8ee3d0f0461e5b48d160e2be1dbca42df694
stroherdebora <stroherdebora@gmail.com>
2022-05-20 13:08:29 -0300

    add messages

---
+++ lib/messages.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Messages extends StatefulWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  State<Messages> createState() => _MessagesState();
+}
+
+class _MessagesState extends State<Messages> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Messages',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text('$_counter', style: Theme.of(context).textTheme.headline4),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (_incrementCounter),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
